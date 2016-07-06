64ee5de4c7d8aa010c4782988fb0f22658e03486
Nick Olds <nolds9@yahoo.com>
2016-07-05 20:56:23 -0400

    can update app state

--- app/index.js
+++ app/index.js
@@ -1,41 +1,30 @@
 $(document).ready(function (){
-  console.log("Ready, Set, Jact");
+  console.log('Ready, Set, Jact');
   class App {
     constructor() {
       this.el = $('#app')
       this.state = {
         greetings: [
-          "Hello There",
-          "Howdy",
-          "Como Estas"
+          'Hello There',
+          'Howdy',
+          'Como Estas'
         ]
       };
     }
-    // TODO whenever a state change occurs, update state, then rerender app
-    // updateState (state) {
-    //   this.state = {
-    //     ...this.state,
-    //     ...state
-    //   }
-    //   this.render()
-    // }
+    updateState (state) {
+      this.state = Object.assign(this.state, state)
+      this.render()
+    }
     renderGreetings () {
       let html = '';
       for (let i = 0; i < this.state.greetings.length; i++) {
         html += `<h${i + 1}>${this.state.greetings[i]}</h${i + 1}>`
       }
-      console.log(html);
       return html
     }
-
     render () {
-      let html = "";
+      let html = '';
       html = this.renderGreetings();
-      console.log(html);
-      // this.el.html(function (i, oldHtml ) {
-      //   // TODO: detect if state change
-      //   // if so diff oldhtml with new html
-      // })
       this.el.html(html)
     }
   }
